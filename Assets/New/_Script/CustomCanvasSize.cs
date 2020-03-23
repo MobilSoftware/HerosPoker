8ea4e9a55b8fdc5ed09c379cb928d955b92d67b1
Michael <michael@myplay.id>
2020-03-23 11:59:30 +0700

    custom canvas

---
+++ Assets/New/_Script/CustomCanvasSize.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CustomCanvasSize : MonoBehaviour {
+
+    public float desiredRatio = 1.7778f;
+
+	void Start () {
+        CanvasScaler cs = this.GetComponent<CanvasScaler>();
+        int width = Screen.width;
+        int height = Screen.height;
+        float currentRatio = (float) (height / width);
+        if (currentRatio > desiredRatio)
+        {
+            cs.matchWidthOrHeight = 0;
+        } else if (currentRatio < desiredRatio)
+        {
+            cs.matchWidthOrHeight = 1;
+        } else
+        {
+            cs.matchWidthOrHeight = 0.5f;
+        }
+	}
+
+}
