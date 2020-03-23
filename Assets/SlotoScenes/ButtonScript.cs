9ead90b272602d83364a29afbef8d96299638baa
Michael <michael@myplay.id>
2020-03-23 10:18:14 +0700

    prototype

--- Assets/SlotoScenes/ButtonScript.cs
+++ Assets/SlotoScenes/ButtonScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ButtonScript : MonoBehaviour
 {
-    public enum ButtonType {Spin, Stop, Max, Up, Down, Back};
+    public enum ButtonType {Spin, Stop, Max, Up, Down, Back, Plus};
 
     public ButtonType type;
 
--- Assets/SlotoScenes/SlotoManagerScript.cs
+++ Assets/SlotoScenes/SlotoManagerScript.cs
@@ -11,6 +11,7 @@ public class SlotoManagerScript : MonoBehaviour
     public TextMesh betLabelTM;
     public TextMesh currBetLabelTM;
     public TextMesh currMoneyLabelTM;
+    public TextMesh winningBetAnimTM;
     public ReelScript[] reels;
     public JackpotReelScript jackpotReel;
     public IconScript[] slotIcons;
@@ -20,48 +21,57 @@ public class SlotoManagerScript : MonoBehaviour
 
     private int bet;
     private int currBet;
+    private int betWinning;
+    private int currBetWinning;
     private int money;
     private int currMoney;
     private int[] showLine = new int[5];
     private int updateSlotSpin;
 
-    // Start is called before the first frame update
     void Start()
     {
         Init();
+        SetMoney();
     }
 
     private void Init()
     {
-        //money = 10000;
-        //currMoney = 10000;
-
         bet = 100;
-        betLabelTM.text = bet.toShortCurrency ();
+        betWinning = 0;
+        currBetWinning = 0;
+        betLabelTM.text = bet.ToString("N0");
         currBet = bet;
+        winningBetAnimTM.gameObject.SetActive(false);
         for (int i = 0; i < reels.Length; i++)
         {
             reels[i].Init(tileSprite, iconSprite);
         }
     }
 
-    public void SetMoney ()
+    public void SetMoney()
     {
         int ownedGold = System.Convert.ToInt32 (PlayerData.owned_gold);
+        //int ownedGold = System.Convert.ToInt32(10000);
         money = ownedGold;
         currMoney = ownedGold;
-        currMoneyLabelTM.text = currMoney.toShortCurrency ();
+        currMoneyLabelTM.text = ToShortCurrency(currMoney);
     }
 
-    // Update is called once per frame
     void Update()
     {
         if(currMoney != money)
         {
             int temp = Mathf.CeilToInt(Mathf.Abs(money - currMoney) / 10.0f);
             currMoney += (currMoney < money ? temp : -temp);
-            //currMoneyLabelTM.text = currMoney.ToString("N0");
-            currMoneyLabelTM.text = currMoney.toShortCurrency ();
+            currMoneyLabelTM.text = ToShortCurrency(currMoney);
+        }
+        if (currBetWinning != betWinning)
+        {
+            int temp = Mathf.CeilToInt(Mathf.Abs(betWinning - currBetWinning) / 10.0f);
+            currBetWinning += (currBetWinning < betWinning ? temp : -temp);
+            currBetLabelTM.text = currBetWinning.ToString("N0");
+            winningBetAnimTM.text = currBetLabelTM.text;
+            if (currBetWinning == betWinning) StartBlink(currBetLabelTM.gameObject);
         }
     }
 
@@ -71,7 +81,6 @@ public class SlotoManagerScript : MonoBehaviour
         {
             if(money >= bet)
             {
-                money -= bet;
                 StartCoroutine(StartSpin());
             }
         } else if (type == ButtonScript.ButtonType.Stop)
@@ -81,28 +90,27 @@ public class SlotoManagerScript : MonoBehaviour
         else if (type == ButtonScript.ButtonType.Max)
         {
             bet = 10000;
-            //betLabelTM.text = bet.ToString("N0");
-            betLabelTM.text = bet.toShortCurrency ();
+            betLabelTM.text = bet.ToString("N0");
         }
         else if (type == ButtonScript.ButtonType.Up)
         {
             bet = Mathf.Clamp(bet + 100, 100, 10000);
-            //betLabelTM.text = bet.ToString("N0");
-            betLabelTM.text = bet.toShortCurrency ();
+            betLabelTM.text = bet.ToString("N0");
         }
         else if (type == ButtonScript.ButtonType.Down)
         {
             bet = Mathf.Clamp(bet - 100, 100, 10000);
-            //betLabelTM.text = bet.ToString("N0");
-            betLabelTM.text = bet.toShortCurrency ();
+            betLabelTM.text = bet.ToString("N0");
         }
         else if (type == ButtonScript.ButtonType.Back)
         {
-            Debug.Log("Replace this function at SlotoManagerScript");
-            //Application.Quit();
             PlayerData.owned_gold = System.Convert.ToInt32 (money);
             _SceneManager.instance.SetActiveScene (SceneType.HOME, true);
             _SceneManager.instance.SetActiveScene (SceneType.SLOTO, false);
+        }
+        else if (type == ButtonScript.ButtonType.Plus)
+        {
+            Debug.Log("Insert Plus Code Here");
         }
     }
 
@@ -110,11 +118,11 @@ public class SlotoManagerScript : MonoBehaviour
     {
         if (reels[0].spin == 0 && !jackpotReel.GetSpin())
         {
+            money -= bet;
             updateSlotSpin = 0;
             HideLine();
+            winningBetAnimTM.gameObject.SetActive(false);
             currBet = bet;
-            //currBetLabelTM.text = currBet.ToString("N0");
-            currBetLabelTM.text = currBet.toShortCurrency ();
             for (int i = 0; i < slotIcons.Length; i++)
             {
                 slotIcons[i].fixedValue = false;
@@ -135,7 +143,7 @@ public class SlotoManagerScript : MonoBehaviour
             for (int i = 0; i < reels.Length; i++)
             {
                 reels[i].StopSpin();
-                yield return new WaitForSeconds(0.1f * Random.Range(1, 11));
+                yield return new WaitForSeconds(0.05f * Random.Range(1, 11));
             }
         }
     }
@@ -147,17 +155,14 @@ public class SlotoManagerScript : MonoBehaviour
         int rand = Random.Range(0, 3);
         if (rand == 0)
         {
-            //tempValue = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
             showLine = new int[] { 1 + (Random.Range(0, 3) * 5), 2 + (Random.Range(0, 3) * 5), 3 + (Random.Range(0, 3) * 5) };
         }
         else if (rand == 1)
         {
-            //tempValue = new int[] { 0, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3 };
             showLine = new int[] { 1 + (Random.Range(0, 3) * 5), 2 + (Random.Range(0, 3) * 5), 3 + (Random.Range(0, 3) * 5), 4 + (Random.Range(0, 3) * 5) };
         }
         else if (rand == 2)
         {
-            //tempValue = new int[] { 2, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4 };
             showLine = new int[] { 1 + (Random.Range(0, 3) * 5), 2 + (Random.Range(0, 3) * 5), 3 + (Random.Range(0, 3) * 5), 4 + (Random.Range(0, 3) * 5), 5 + (Random.Range(0, 3) * 5) };
         }
         for (i = 0; i < tempValue.Length; i++)
@@ -201,6 +206,34 @@ public class SlotoManagerScript : MonoBehaviour
 
     public void UpdateMoney()
     {
-        money += (100 * showLine.Length);
+        betWinning = (100 * showLine.Length);
+        money += betWinning;
+        winningBetAnimTM.gameObject.SetActive(true);
+    }
+
+    private string ToShortCurrency(int value)
+    {
+        return value.toShortCurrency();
+        //return value.ToString("N0");
+    }
+
+    private void StartBlink(GameObject go)
+    {
+        StartCoroutine(StartBlinking(go));
+    }
+
+    IEnumerator StartBlinking(GameObject go)
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            go.SetActive(false);
+            winningBetAnimTM.gameObject.SetActive(false);
+            yield return new WaitForSeconds(0.5f);
+            go.SetActive(true);
+            winningBetAnimTM.gameObject.SetActive(true);
+            yield return new WaitForSeconds(0.5f);
+        }
+        yield return new WaitForSeconds(1.0f);
+        winningBetAnimTM.gameObject.SetActive(false);
     }
 }
